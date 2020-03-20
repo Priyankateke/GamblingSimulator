7e2b74a73fcf5d5e7d9b1629fc58576e111c904d
Priyankateke <priiyankateke1705@gmail.com>
2020-03-20 07:02:27 -0700

    ADDED: Condition To Check Gambler Win Or Losse Upto DailyLimit

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -5,6 +5,8 @@ echo "Welcome To Gambling Simulation"
 #constants
 readonly EVERY_DAY_STAKE=100
 readonly BET=1
+LOWER_PERCENT=$((EVERY_DAY_STAKE-$((EVERY_DAY_STAKE * 50 /100)) ))
+UPPER_PERCENT=$((EVERY_DAY_STAKE+$((EVERY_DAY_STAKE * 50 /100)) ))
 
 #variables
 cash=$EVERY_DAY_STAKE
@@ -12,14 +14,15 @@ cash=$EVERY_DAY_STAKE
 #Function to check win or loss 
 function dailyBet()
 {
-	if [ $((RANDOM % 2)) -eq 1 ]
-	then
-		echo "Win"
-		cash=$((cash+BET))
-	else
-		echo "Loss"
-		cash=$((cash-BET))
-	fi
+	while [ $cash -gt $LOWER_PERCENT ] && [ $cash -lt $UPPER_PERCENT ]
+	do
+		if [ $((RANDOM % 2)) -eq 1 ]
+		then
+			cash=$((cash+BET))
+		else
+			cash=$((cash-BET))
+		fi
+	done
 }
 
 dailyBet
