11ffbee71e8bdd6019e77c228735442b729b2273
Priyankateke <priiyankateke1705@gmail.com>
2020-03-20 06:47:38 -0700

    ADDED: Checked Gambler Win Or Loss

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -2,6 +2,24 @@
 
 echo "Welcome To Gambling Simulation"
 
-#variables
+#constants
 readonly EVERY_DAY_STAKE=100
 readonly BET=1
+
+#variables
+cash=$EVERY_DAY_STAKE
+
+#Function to check win or loss 
+function dailyBet()
+{
+	if [ $((RANDOM % 2)) -eq 1 ]
+	then
+		echo "Win"
+		cash=$((cash+BET))
+	else
+		echo "Loss"
+		cash=$((cash-BET))
+	fi
+}
+
+dailyBet
